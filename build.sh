715c1be996b5286504f3cf819c6753e82b4f54c5
dongyeul <rlaehdduf04@gmail.com>
2026-04-06 11:43:41 +0900

    fix: Fix DOTNET_ROOT path in build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
 
 # Add .NET to PATH
 export DOTNET_ROOT=$HOME/.dotnet
-export PATH=$DOT_ROOT:$PATH
+export PATH=$DOTNET_ROOT:$PATH
 
 # Build the project
 dotnet publish MiniGameCollection.Web -c Release -o output
